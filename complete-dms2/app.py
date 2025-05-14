f22555351a13f72bea0723f4f85848aee5e069ea
DeenadhayalanM <deenadhayalan.m@corp.knowledgelens.com>
2025-05-14 13:14:22 +0530

    changes

--- complete-dms2/app.py
+++ complete-dms2/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import HTMLResponse
 
 from scripts.services.deployment_service import deployment_router
 from scripts.services.image_service import image_router as image_router
@@ -15,7 +18,46 @@ def create_app(root_path: str = "") -> FastAPI:
         description="APIs to manage Docker Images, Containers, and Volumes",
         version="1.0.0",
         root_path=root_path,
+        docs_url=None,  
+        redoc_url=None,
     )
+
+    @app.get("/docs", include_in_schema=False)
+    async def custom_swagger_ui_html():
+        return get_swagger_ui_html(
+            openapi_url=f"{root_path}/openapi.json",
+            title="Docker Management API Docs",
+            oauth2_redirect_url=f"{root_path}/docs/oauth2-redirect",
+            swagger_ui_parameters={"persistAuthorization": True},
+        )
+
+    @app.get("/openapi.json", include_in_schema=False)
+    async def custom_openapi():
+        return get_openapi(
+            title=app.title,
+            version=app.version,
+            description=app.description,
+            routes=app.routes,
+        )
+
+    @app.get("/docs/oauth2-redirect", include_in_schema=False)
+    async def swagger_redirect():
+        return HTMLResponse(
+            """
+            <script>
+                'use strict';
+                function run () {
+                    var hash = window.location.hash;
+                    var message = {
+                        type: "oauth2_redirect",
+                        hash: hash
+                    }
+                    window.opener.postMessage(message, window.location.origin);
+                }
+                window.onload = run
+            </script>
+            """
+        )
 
     app.add_middleware(
         CORSMiddleware,
@@ -33,5 +75,3 @@ def create_app(root_path: str = "") -> FastAPI:
     app.include_router(deployment_router, prefix="/deployment", tags=["Deployment Operations"])
 
     return app
-
-

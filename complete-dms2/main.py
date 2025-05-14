a053972e82c79b07d2f6908a6133d143fc79f57f
DeenadhayalanM <deenadhayalan.m@corp.knowledgelens.com>
2025-05-14 12:09:36 +0530

    changes

--- complete-dms2/main.py
+++ complete-dms2/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from app import create_app
 from scripts.constants.app_configuration import settings
 
-app = create_app()
+app = create_app(root_path="/app1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host=settings.API_HOST, port=settings.API_PORT)
